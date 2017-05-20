d0ebd04e00afa0843595edf2dc7574dda67d35db
Robin Winslow <robin@robinwinslow.co.uk>
2017-05-20 17:16:20 +0100

    Move error files to top level

--- webapp/views.py
+++ webapp/views.py
@@ -3,11 +3,11 @@ from django.template import RequestContext, loader, Context
 
 
 def custom_404(request):
-    t = loader.get_template('error/404.html')
+    t = loader.get_template('404.html')
     context = RequestContext(request, {'request_path': request.path})
     return HttpResponseNotFound(t.render(context))
 
 
 def custom_500(request):
-    t = loader.get_template('error/500.html')
+    t = loader.get_template('500.html')
     return HttpResponseServerError(t.render(Context({})))
