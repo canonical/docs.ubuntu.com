aa90cf9985549e5b79abd211411142c69802f3ea
João Martins <joao.figueiredo.martins@canonical.com>
2018-10-31 11:05:08 +0000

    Redirect searches to https://www.ubuntu.com/search

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,18 +1,18 @@
 # Modules
 from django.conf.urls import url
-from django_yaml_redirects import load_redirects
 from django_template_finder_view import TemplateFinder
-from canonicalwebteam.gsa.views import SearchView
+from canonicalwebteam.yaml_responses.django_helpers import (
+    create_redirect_views
+)
 
 # Local
 from webapp.views import custom_404, custom_500
 
 # Match any redirects first
-urlpatterns = load_redirects()
+urlpatterns = create_redirect_views()
 
 # Try to find templates
 urlpatterns += [
-    url(r'^search/?$', SearchView.as_view(template_name='search.html')),
     url(r'^(?P<template>.*)/?$', TemplateFinder.as_view())
 ]
 
