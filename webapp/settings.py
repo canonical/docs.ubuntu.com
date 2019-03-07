aeb142ffd6a6665c539b350381fec467c9095dce
Robin Winslow <robin@robinwinslow.co.uk>
2019-03-07 11:59:01 +0000

    Use default logging settings

--- webapp/settings.py
+++ webapp/settings.py
@@ -83,26 +83,5 @@ TEMPLATES = [
                 'django_asset_server_url.asset_server_url',
             ],
         },
-    },
-]
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'error_file': {
-            'level': 'ERROR',
-            'filename': os.path.join(BASE_DIR, 'django-error.log'),
-            'class': 'logging.handlers.RotatingFileHandler',
-            'maxBytes': 1 * 1024 * 1024,
-            'backupCount': 2
-        }
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['error_file'],
-            'level': 'ERROR',
-            'propagate': True
-        }
     }
-}
+]
