378cd8ba0940583fa9c5b09696829c7f90fe3a65
nbellol <nbellolugo97@gmail.com>
2026-01-23 16:18:17 -0500

    fix formatting urls

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,16 +1,14 @@
 # Modules
 from django.urls import re_path
 from django_template_finder_view import TemplateFinder
-
-
-# Compatibility shim for older packages expecting django.conf.urls.url
-from django.conf import urls as django_urls
-django_urls.url = re_path
-
-
 from canonicalwebteam.yaml_responses.django_helpers import (
     create_redirect_views,
 )
+
+# Compatibility shim for older packages expecting django.conf.urls.url
+from django.conf import urls as django_urls
+
+django_urls.url = re_path
 
 
 # Match any redirects first
